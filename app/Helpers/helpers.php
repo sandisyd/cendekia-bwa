8e5309ae7f63ce3d7db77ea867317c0f607011f1
Sandi <sandidwi367@gmail.com>
2026-01-02 20:28:10 +0700

    create helpers and running autoload

---
+++ app/Helpers/helpers.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Models\User;
+
+if(!function_exists('flashMessage')){
+    function flashMessage($message, $type = 'success'):void
+    {
+        session()->flash('message', $message);
+        session()->flash('type', $type);
+    }
+}
+
+if(!function_exists('usernameGenerator')){
+    function usernameGenerator(string $name): string
+    {
+        $username = strtolower(preg_replace('/\s+/', '_', trim($name)));
+        $original_username = $username;
+
+        $count = 1;
+
+        while(User::where('username', $username)->exists())
+        {
+            $username = $original_username . $count;
+            $count++;
+        }
+
+        return $username;
+    }
+}
+
+if(!function_exists('signatureMidtrans')){
+    function signatureMidtrans($orderId, $statusCode, $grossAmount, $serverKey)
+    {
+        return hash('sha512', $orderId . $statusCode . $grossAmount . $serverKey);
+    }
+}
