fe7fe8ac6a8a571485301528ca0156b73e96fe5e
Sandi <sandidwi367@gmail.com>
2026-01-21 09:03:49 +0700

    update layout

---
+++ resources/js/Pages/AdminDashboard.jsx
@@ -0,0 +1,5 @@
+import AppLayout from "@/Layouts/AppLayout";
+
+export default function AdminDashboard() {
+  return <AppLayout title="Dashboard">Sedang testing</AppLayout>;
+}
--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@ Route::get('/', function () {
     ]);
 });
 
+Route::get('/admin-dashboard', fn() => inertia('AdminDashboard'));
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
